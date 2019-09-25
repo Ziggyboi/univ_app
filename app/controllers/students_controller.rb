833c1d3b18d4b4ca3c35a5654aa43e24939069f1
Ziggyboi <dumbleshoot@gmail.com>
2019-09-25 14:11:22 +0100

    Final course commit

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,7 @@
 class StudentsController < ApplicationController
     before_action :set_student, only: [:show, :edit, :update]
-
+    skip_before_action :require_user, only: [:new, :create]
+    before_action :require_same_student, only: [:edit, :update]
     def index
         @students = Student.all
     end
@@ -44,5 +45,12 @@ class StudentsController < ApplicationController
 
     def set_student
         @student = Student.find(params[:id])
+    end
+    
+    def require_same_student
+        if current_user != @student    
+            flash[:notice] = "You can only edit your own profile" 
+            redirect_to student_path(current_user)
+        end
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+    def session_link
+        if (logged_in?)
+           link_to "logout", logout_path, method: :delete 
+        else
+           link_to "login", login_path
+        end
+    end
+
+    def require_user
+        if !logged_in?
+            flash[:notice] = "You must be logged in to perform that action"
+            redirect_to login_path
+        end
+    end
 end
